eda65dd26f4774140da4a88a6ea1bfdd1315fe04
reisdlucas <reisdlucas@icloud.com>
2022-03-23 13:08:12 -0300

    Abrindo Conta

---
+++ src/bytebank/Conta.java
@@ -0,0 +1,11 @@
+// Class tipo Conta:
+
+package bytebank;
+
+public class Conta {
+// Especificação de atributo:
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+}
---
+++ src/bytebank/CriaConta.java
@@ -0,0 +1,14 @@
+// Objeto do tipo: Conta
+package bytebank;
+
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		// Conta <- do tipo <- variével:firstaccount = new Conta()	
+		Conta firstAccount = new Conta();
+		// firstAccount[.] <- Ponto para navegar pelos atributos: Conta 
+		firstAccount.saldo = 200;		
+		System.out.println(firstAccount.saldo);
+	}
+	
+}
