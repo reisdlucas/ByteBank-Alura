8e2575709a04296c00784ad0fe0bf041a1b58e33
reisdlucas <reisdlucas@icloud.com>
2022-03-24 14:02:09 -0300

    Criação Cliente

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -6,7 +6,8 @@ public class Conta {
 	double saldo;
 	int agencia;
 	int numero;
-	String titular;
+// Referência para o objeto:
+	Cliente titular;
 	
 // Métodos:
 	
---
+++ src/bytebank/TestBank.java
@@ -0,0 +1,20 @@
+package bytebank;
+
+public class TestBank {
+	
+	public static void main(String[] args) {
+		Cliente lucas = new Cliente();
+		lucas.nome = "Lucas Reis";
+		lucas.cpf = "117.262.907-27";
+		lucas.profissao = "Programador";
+		
+		Conta contaLucas = new Conta();
+		contaLucas.deposita(100);
+		
+// Atribuição de referência para o atributo Titular:
+		
+		contaLucas.titular = lucas;
+		
+	}
+	
+}
