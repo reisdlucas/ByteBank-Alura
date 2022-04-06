6f9c789cb36f89e920febd9ded93cb4d9b8e4406
reisdlucas <reisdlucas@icloud.com>
2022-04-06 14:48:04 -0300

    Começando Exceções - Try/Catch

---
+++ src/bytebank/TryCatch.java
@@ -0,0 +1,36 @@
+package bytebank;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ini da main");
+
+		try {
+
+			metodo1();
+
+		} catch (ArithmeticException | NullPointerException ex) {
+
+			String msg = ex.getMessage();
+			System.out.println("Exception" + msg);
+			ex.printStackTrace();
+		} 
+		
+		System.out.println("Fim da main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Ini do metodo 1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo 2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+		}
+	}
+}
