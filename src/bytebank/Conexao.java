8f765338baf6f18f4f39e7e195e9b5bf27f1da1a
reisdlucas <reisdlucas@icloud.com>
2022-04-07 11:02:30 -0300

    Finally

---
+++ src/bytebank/Conexao.java
@@ -0,0 +1,16 @@
+package bytebank;
+
+public class Conexao {
+	
+	public Conexao() {
+		System.out.println("Abrindo conexao");		
+	}
+	
+	public void leDados() {
+		System.out.println("Recebendo dados");
+	}
+	
+	public void fecha() {
+		System.out.println("Fechando conexao");
+	}
+}
